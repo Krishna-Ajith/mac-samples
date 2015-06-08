cf0d5f5ead5695751b2b0e501bea9211d7af1698
Krishna-Ajith <tkrishnaajith@gmail.com>
2015-06-08 11:34:12 +0530

    implemented #45

--- config.mk
+++ config.mk
@@ -1,3 +1,4 @@
 MDROOT = $(shell stat -f%N "/Applications/Xamarin Studio.app" 2>/dev/null || echo "/Applications/MonoDevelop.app")
 MDROOT_ESCAPED = $(shell echo "$(MDROOT)" | sed 's| |\\ |g')
 MDTOOL = $(MDROOT)/Contents/MacOS/mdtool
+vcbcxbxbxv
